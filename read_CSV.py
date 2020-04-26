1095ffcba3f2909ba9e8ed41b4f484b4f4019c2d
Steivan <steivan@gmx.ch>
2020-04-26 21:23:19 +0200

    progress bar update

--- read_CSV.py
+++ read_CSV.py
@@ -4,22 +4,33 @@ import csv
 import numpy as np
 import datetime
 
-def read_csv(MSID_path, start, end, csv_url):
-    MSID_list = [line.rstrip('\n') for line in open(MSID_path)]
+def read_csv(msid_path, start, end, csv_url):
+    msid_list = [line.rstrip('\n') for line in open(msid_path)]
 
-    I = len(MSID_list)
+    I = len(msid_list)
     J = (end.date() - start.date()).days + 1
     K = 24
     A = np.zeros((I, J, K), dtype=np.int32)
+
+    msid_count = 185
+    start_year = datetime.date(datetime.datetime.now().year, 1, 1)
+    now = datetime.date.today()
+    delta = now - start_year
+    entries_count = msid_count * delta.days * 24 + datetime.now().hour * msid_count
+    progress = 0
 
     with closing(requests.get(csv_url, stream=True)) as r:
         f = (line.decode('utf-8') for line in r.iter_lines())
         reader = csv.reader(f, delimiter=',', quotechar='"')
         for row in reader:
+            progress += 1
+            progress_percent = progress / entries_count * 100
+            #Update progressbar here!
+
             if len(row) != 0:
-                if row[0] in MSID_list:
+                if row[0] in msid_list:
                     if start.isSmaller(row[17]) and end.isBigger(row[17]):
-                        i = MSID_list.index(row[0])
+                        i = msid_list.index(row[0])
                         datetime_now = datetime.datetime.strptime(row[17], '%Y-%m-%dT%H:%M:%S')
                         j = (datetime_now - start.date()).days
                         k = datetime.datetime.strptime(row[17], '%Y-%m-%dT%H:%M:%S').hour
@@ -28,7 +39,7 @@ def read_csv(MSID_path, start, end, csv_url):
     base = start.date()
     date_list = [base + datetime.timedelta(days=x) for x in range(J)]
 
-    return A, MSID_list, date_list
+    return IdCube(A, msid_list, date_list)
 
     # with open('outfile.txt', 'wb') as f:
     #     for line in A:
