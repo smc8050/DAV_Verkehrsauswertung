d76ae0dea21422903f9e43542e3e20fbcb84ccf9
smc8050 <smc8050@hotmail.com>
2020-04-26 17:36:24 +0200

    Crate MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,8 @@
+from read_CSV import read_csv
+from utils import Timestamp
+
+t0 = Timestamp('2020-01-01T00:00:00')
+t1 = Timestamp('2020-01-03T00:00:00')
+ID = ['Z033M002','Z052M002']
+
+read_csv(ID, t0, t1)
