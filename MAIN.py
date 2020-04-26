ab8c2f7f2a93f32821c4664138b86bf6e88f1b89
Steivan <steivan@gmx.ch>
2020-04-26 17:47:57 +0200

    Initial Push

--- MAIN.py
+++ MAIN.py
@@ -3,6 +3,6 @@ from utils import Timestamp
 
 t0 = Timestamp('2020-01-01T00:00:00')
 t1 = Timestamp('2020-01-03T00:00:00')
-ID = ['Z033M002','Z052M002']
+ID = ['Z033M002']
 
 read_csv(ID, t0, t1)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DAV_Verkehrsauswertung
+Dies ist ein Tool welches die Verkehrsdaten der Stadt Zürich verarbeitet.
