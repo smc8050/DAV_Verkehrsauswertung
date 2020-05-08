b2861ca70b23f81b660ee089ea938965e23009a1
tobiac <tobiac@student.ethz.ch>
2020-05-08 10:37:18 +0200

    added two bools for weekends and holidays

--- dav_auswertung.py
+++ dav_auswertung.py
@@ -1,5 +1,5 @@
 from read_CSV import read_csv
-from plot import *
+from plot import PlotterContext, DefaultPlotter, ExtendedPlotter
 
 class DavAuswertung:
     '''
@@ -9,8 +9,8 @@ class DavAuswertung:
     def __init__(self, save_path, csv_url ,msid_list_path,start_date,end_date, msp_asp, do_plots, exclude_weekends, exclude_holidays):
         result_IdCube = read_csv(msid_list_path, start_date, end_date, csv_url, exclude_weekends, exclude_holidays)
         if msp_asp:
-            pl = PlotterContext(ExtendedPlotter(save_path, result_IdCube, do_plots))
+            pl = PlotterContext(ExtendedPlotter(save_path, result_IdCube, do_plots, exclude_weekends, exclude_holidays))
         else:
-            pl = PlotterContext(DefaultPlotter(save_path, result_IdCube, do_plots))
+            pl = PlotterContext(DefaultPlotter(save_path, result_IdCube, do_plots, exclude_weekends, exclude_holidays))
 
         pl.plot()
